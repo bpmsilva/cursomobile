b6d9ad53c077d72a1cd162644c83894a5a7f74ec
Bernardo <bpmsilva@gmail.com>
2026-09-13 12:38:35 -0300

    arquivo java sobre variáveis

---
+++ src/java/03-variaveis/Main.java
@@ -0,0 +1,25 @@
+class Main {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        int idade = 25;       // Variável inteira
+        double altura = 1.75; // Variável de ponto flutuante (números com casas decimais)
+        String nome = "João"; // Variável de texto
+
+        /**
+         * São oito tipos primitivos em Java (String não é um tipo primitivo, mas sim uma classe):
+         * boolean: true ou false
+         * int: um número inteiro de 32 bits, por exemplo 42
+         * char: um único caractere, por exemplo 'A'
+         * byte: um número inteiro de 8 bits, por exemplo 127
+         * short: um número inteiro de 16 bits, por exemplo 32767
+         * long: um número inteiro de 64 bits, por exemplo 922337203685
+         * float: um número de ponto flutuante de 32 bits, por exemplo 3.14f
+         * double: um número de ponto flutuante de 64 bits, por exemplo 3.14159
+         */
+
+        // Exibindo os valores das variáveis
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+    }
+}
