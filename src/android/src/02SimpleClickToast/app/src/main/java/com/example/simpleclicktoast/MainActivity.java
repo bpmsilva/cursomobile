8b16a44c585e5b310795aada0a0d0fc20360f72e
Bernardo <bpmsilva@gmail.com>
2026-09-15 14:13:21 -0300

    adiciona segundo projeto android: contador

---
+++ src/android/src/02SimpleClickToast/app/src/main/java/com/example/simpleclicktoast/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.simpleclicktoast;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int contagem = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void contar(View view) {
+
+        // incrementa contador
+        contagem += 1;
+
+        // Exibe resultado
+        TextView contador = findViewById(R.id.textoContador);
+        contador.setText("Contagem: " + contagem);
+    }
+}
